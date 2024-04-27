f9ad812d5136e3797c4a3fb630cd6f96c6a46f54
Nightmare Corporation <12363909-nightmarecorporation@users.noreply.gitlab.com>
2024-04-27 12:05:36 +0200

    feat: comp for plants to transform on maturity

---
+++ Source/Terrasecurity/PlantTransformOnMaturity/Patch_Plant.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Terrasecurity
+{
+    [HarmonyPatch(typeof(Plant), nameof(Plant.TickLong))]
+    public static class Patch_Plant
+    {
+        [HarmonyPostfix]
+        public static void CallTransformComp(Plant __instance)
+        {
+            try
+            {
+                ThingComp_PlantTransformOnMaturity comp = __instance.GetComp<ThingComp_PlantTransformOnMaturity>();
+                if(comp == null)
+                {
+                    return;
+                }
+                comp.TransformTickLong();
+            }
+            catch(Exception ex)
+            {
+                Log.Error($"Caught unhandled exception: {ex}");
+            }
+        }
+    }
+}
---
+++ Source/Terrasecurity/PlantTransformOnMaturity/ThingComp_PlantTransformOnMaturity.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Terrasecurity
+{
+    public class ThingComp_PlantTransformOnMaturity : ThingComp
+    {
+        ThingCompProperties_PlantTransformOnMaturity Props => base.props as ThingCompProperties_PlantTransformOnMaturity;
+
+        /// <summary>
+        /// Can't use the CompTickLong. Destroying the parent in the method causes NRE later on, so this method is called from a patch.
+        /// </summary>
+        //public override void CompTickLong()
+        public void TransformTickLong()
+        {
+            base.CompTickLong();
+            if(!(parent is Plant plant))
+            {
+                return;
+            }
+            if(plant.LifeStage == PlantLifeStage.Mature)
+            {
+                Transform();
+            }
+        }
+
+        private void Transform()
+        {
+            Map map = parent.Map;
+            parent.Destroy(DestroyMode.WillReplace);
+            Thing thingToSpawn = Props.MakeThing();
+            GenSpawn.Spawn(thingToSpawn, parent.Position, map, WipeMode.FullRefund);
+        }
+    }
+}
